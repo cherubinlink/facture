01cbc99a66c01ea3acbf7227aaa0526cc58db491
cherubinlink <entrpriselinks@gmail.com>
2024-12-30 12:04:35 +0100

    front-end

--- fact/fact_gest/urls.py
+++ fact/fact_gest/urls.py
@@ -9,6 +9,24 @@ urlpatterns = [
     # accueil user
     path('accueil_user',views.accuei_user,name='accueil-user'),
     
+    # entreprise
+    path('entreprise',views.entreprise,name='entreprise'),
+    
     # produit
-    path('produit',views.produit,name='produit')
+    path('produit',views.produit,name='produit'),
+    
+    # services
+    path('services',views.service,name='service'),
+    
+    # client
+    path('client',views.client,name='client'),
+    
+    # facture
+    path('facture',views.facture,name='facture'),
+    
+    # facture paye
+    path('facture_paye',views.facture_paye,name='facture-paye'),
+    
+    # facture nom paye
+    path('facture_nom_paye',views.facture_nom_paye,name='facture-nom-paye')
 ]
--- fact/fact_gest/views.py
+++ fact/fact_gest/views.py
@@ -2,14 +2,42 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+# accueil principal
 def accueil(request):
     return render(request,'fact_gest/accueil.html')
 
-
+# accueil utiliateur 
 def accuei_user(request):
     return render(request,'fact_gest/accueil_user.html')
 
+# entreprise
+def entreprise(request):
+    return render(request,'fact_gest/entreprise.html')
 
+# produits
 def produit(request):
     return render(request,'fact_gest/produit.html')
+
+# services
+def service(request):
+    return render(request,'fact_gest/service.html')
+
+
+# clients
+def client(request):
+    return render(request,'fact_gest/client.html')
+
+# facture
+def facture(request):
+    return render(request,'fact_gest/facture.html')
+
+# facture paye
+def facture_paye(request):
+    return render(request,'fact_gest/facture_paye.html')
+
+
+# facture nom paye
+def facture_nom_paye(request):
+    return render(request,'fact_gest/facture_nom_paye.html')
+
+
